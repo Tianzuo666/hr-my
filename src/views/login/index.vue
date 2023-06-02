c9ec0de2ef764134f9692bed6c2682888a97d67a
tianzuoshi <1007047957@qq.com>
2023-06-02 11:26:17 +0800

    第一次提交

---
+++ src/views/login/index.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="login-container">
+    <div class="logo" />
+    <div class="form">
+      <h1>登录</h1>
+      <el-card shadow="never" class="loginCard">
+        <!--登录表单-->
+        <el-form
+          ref="ruleForm"
+          :model="ruleForm"
+          :rules="rules"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
+          <el-form-item label="活动名称" prop="name">
+            <el-input v-model="ruleForm.name" />
+          </el-form-item>
+          <el-form-item>
+            <el-button
+              type="primary"
+              @click="submitForm('ruleForm')"
+            >立即创建</el-button>
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-card>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      ruleForm: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        region: [
+          { required: true, message: '请选择活动区域', trigger: 'change' }
+        ],
+        date1: [
+          {
+            type: 'date',
+            required: true,
+            message: '请选择日期',
+            trigger: 'change'
+          }
+        ],
+        date2: [
+          {
+            type: 'date',
+            required: true,
+            message: '请选择时间',
+            trigger: 'change'
+          }
+        ],
+        type: [
+          {
+            type: 'array',
+            required: true,
+            message: '请至少选择一个活动性质',
+            trigger: 'change'
+          }
+        ],
+        resource: [
+          { required: true, message: '请选择活动资源', trigger: 'change' }
+        ],
+        desc: [{ required: true, message: '请填写活动形式', trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    // 提交表单
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    // 重置表单
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
+<style lang="scss">
+.login-container {
+  display: flex;
+  align-items: stretch;
+  height: 100vh;
+  .logo {
+    flex: 3;
+    background: url(../../assets/common/login_back.png) no-repeat center / cover;
+    border-top-right-radius: 60px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: center;
+    padding: 0 100px;
+    .icon {
+      background: url(../../assets/common/logo.png) no-repeat 70px center /
+        contain;
+      width: 300px;
+      height: 50px;
+      margin-bottom: 50px;
+    }
+    p {
+      color: #fff;
+      font-size: 18px;
+      margin-top: 20px;
+      width: 300px;
+      text-align: center;
+    }
+  }
+  .form {
+    flex: 2;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 80px;
+    .el-card {
+      border: none;
+      padding: 0;
+      background-color: skyblue;
+    }
+    h1 {
+      padding-left: 20px;
+      font-size: 24px;
+    }
+    .el-input {
+      width: 350px;
+      height: 44px;
+      .el-input__inner {
+        background: #f4f5fb;
+      }
+    }
+    .el-checkbox {
+      color: #606266;
+    }
+  }
+}
+</style>
